390d3cb35ff6f41adbbe9740c217684974e26667
github username <github email address>
2017-08-27 10:07:46 -0400

    Added distance in provider form component -temporary placeholder

--- src/App.js
+++ src/App.js
@@ -13,15 +13,6 @@ export default class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <form>
-            Find internets within
-            <input type="number" id="distance" value="1" required="required" />
-            <select id="measure">
-              <option value="miles">mile(s)</option>
-              <option value="kilometers">kilometer(s)</option>
-            </select>
-            of here.<br></br>
-          </form>
           <ProviderForm />
           <SimpleMap />
           <Marker />
--- src/components/provider_form.js
+++ src/components/provider_form.js
@@ -9,26 +9,43 @@ export default class ProviderForm extends Component {
   render() {
     return (
       <div>
-        <label>Filter By Provider:</label>
         <Form ref='simpleForm'>
-            <Field
-              name='Time Warner (Spectrum)'
-              label='Time Warner (Spectrum)'
-              id='time-warner'
-              type='checkbox'
-            />
-            <Field
-              name='Cablevision (Altice USA)'
-              label='Cablevision (Altice USA)'
-              id='cablevision'
-              type='checkbox'
-            />
-            <Field
-              name='AT&T'
-              label='AT&T'
-              id='att'
-              type='checkbox'
-            />
+          <label>Find internets within</label>
+          <Field
+            id='distance'
+            value='1'
+            validators={['required']}
+          />
+          <Field
+            id='measure'
+            value='miles'
+            label='Mile(s)'
+          />
+          <Field
+            id='measure'
+            value='kilometers'
+            label='Kilometer(s)'
+          />
+          <p>of here.</p>
+          <label>Filter By Provider:</label>
+          <Field
+            name='Time Warner (Spectrum)'
+            label='Time Warner (Spectrum)'
+            id='time-warner'
+            type='checkbox'
+          />
+          <Field
+            name='Cablevision (Altice USA)'
+            label='Cablevision (Altice USA)'
+            id='cablevision'
+            type='checkbox'
+          />
+          <Field
+            name='AT&T'
+            label='AT&T'
+            id='att'
+            type='checkbox'
+          />
         </Form>
         <button onClick={this.onClickHandler.bind(this)}>FIND INTERNET!</button>
       </div>
